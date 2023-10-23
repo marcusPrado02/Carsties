7b7bd18831b13399eae3103d073da5d3a7662fa5
Marcus Prado Silva <silvamarcusprado@gmail.com>
2023-10-22 22:57:29 -0300

    End of Section 2

---
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -0,0 +1,103 @@
+﻿using AuctionService.Data;
+using AuctionService.DTOs;
+using AuctionService.Entities;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuctionService.Controllers;
+
+[ApiController]
+[Route("api/auctions")]
+public class AuctionsController : ControllerBase
+{
+    private readonly AuctionDbContext _context;
+    private readonly IMapper _mapper;
+    
+    public AuctionsController(AuctionDbContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+    
+    [HttpGet]
+    public async Task<ActionResult<List<AuctionDTO>>> GetAllAuctions()
+    {
+        var auctions = await _context.Auctions
+            .Include(x => x.Item)
+            .OrderBy(x => x.Item)
+            .ToListAsync();
+
+        return  _mapper.Map<List<AuctionDTO>>(auctions);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<AuctionDTO>> GetAuctionById(Guid id)
+    {
+        var auction = await _context.Auctions
+            .Include(x => x.Item)
+            .FirstOrDefaultAsync(x => x.Id == id);
+
+        if(auction == null) return NotFound();
+
+        return _mapper.Map<AuctionDTO>(auction);        
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<AuctionDTO>> CreateAuction(CreateAuctionDTO auctionDTO)
+    {
+        var auction = _mapper.Map<Auction>(auctionDTO);
+        // TODO: add current user as seller
+        auction.Seller = "test";
+
+        _context.Auctions.Add(auction);
+
+        var result = await _context.SaveChangesAsync() > 0;
+
+        if(!result) return BadRequest("Não foi possível salvar as alterações no Banco de Dados.");
+
+        return CreatedAtAction(nameof(GetAuctionById), new {auction.Id}, _mapper.Map<AuctionDTO>(auction));
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult> UpdateAuction(Guid id, UpdateAuctionsDTO updateAuctionsDTO)
+    {
+        var auction = await _context.Auctions.Include(x => x.Item)
+        .FirstOrDefaultAsync(x => x.Id == id);
+
+        if(auction == null) return NotFound();
+
+        // TODO: check seller username
+
+        auction.Item.Make = updateAuctionsDTO.Make ?? auction.Item.Make;
+        auction.Item.Model = updateAuctionsDTO.Model ?? auction.Item.Model;
+        auction.Item.Color = updateAuctionsDTO.Color ?? auction.Item.Color;
+        auction.Item.Mileage = updateAuctionsDTO.Mileage ?? auction.Item.Mileage;
+        auction.Item.Year = updateAuctionsDTO.Year ?? auction.Item.Year;
+        
+        var result = await _context.SaveChangesAsync() > 0;
+        
+        if(result) return Ok(); 
+
+        return BadRequest("Problema ao salvar mudanças.");
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> DeleteAuction(Guid id)
+    {
+        var auction = await _context.Auctions.FindAsync(id);
+
+        if(auction == null) return NotFound();
+
+        // TODO: check seller == username
+
+        _context.Auctions.Remove(auction);
+
+        var result = await _context.SaveChangesAsync() > 0;
+
+        if(!result) return BadRequest("Não foi possível deletar registro.");
+
+        return Ok();
+    }
+    
+}
---
+++ src/AuctionService/Data/Migrations/20231022224930_UpdateItems.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuctionService.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateItems : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Items",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Mileage",
+                table: "Items",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Items");
+
+            migrationBuilder.DropColumn(
+                name: "Mileage",
+                table: "Items");
+        }
+    }
+}
