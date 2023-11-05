71d94d88ce0faf9fe0cce3a0747c38ac5ca92e1e
Marcus Prado Silva <silvamarcusprado@gmail.com>
2023-11-05 19:57:57 -0300

    Update Auction Created

--- src/Contracts/AuctionCreated.cs
+++ src/Contracts/AuctionCreated.cs
@@ -8,14 +8,14 @@ public class AuctionCreated
     public string Winner { get; set; }
     public int SoldAmount { get; set; }
     public int CurrentHighBid { get; set; }
-    public DateTime CreatedAt { get; set; } 
-    public DateTime UpdateAt { get; set; } 
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdateAt { get; set; }
     public DateTime AuctionEnd { get; set; }
     public string Status { get; set; }
     public string Make { get; set; }
     public string Model { get; set; }
     public int Year { get; set; }
     public string Color { get; set; }
-    public int Mileage { get; internal set; }
-    public string ImageUrl { get; internal set; }
+    public int Mileage { get; set; }
+    public string ImageUrl { get; set; }
 }
