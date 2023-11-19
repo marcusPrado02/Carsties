14f41d8bcac9a639af8d8ba85ffff2c4b1424b1c
Marcus Prado Silva <silvamarcusprado@gmail.com>
2023-11-19 18:09:28 -0300

    auctions update

--- frontend/web-app/app/auctions/AuctionCard.tsx
+++ frontend/web-app/app/auctions/AuctionCard.tsx
@@ -1,29 +1,29 @@
-import React from 'react';
-import CountdownTimer from './CountdownTimer';
-import CarImage from './CarImage';
-import { Auction } from '@/types';
+import React from 'react'
+import CountdownTimer from './CountdownTimer'
+import CarImage from './CarImage'
+import { Auction } from '@/types'
+import Link from 'next/link'
 
 type Props = {
-  auction: Auction;
-};
+    auction: Auction
+}
 
 export default function AuctionCard({ auction }: Props) {
-  return (
-    <a href='#' className='group'>
-      <div className='aspect-h-10 aspect-w-16 w-full overflow-hidden rounded-lg bg-gray-200'>
-        <div>
-          <CarImage imageUrl={auction.imageUrl} />
-          <div className='absolute bottom-2 left-2'>
-            <CountdownTimer auctionEnd={auction.auctionEnd} />
-          </div>
-        </div>
-      </div>
-      <div className='mt-4 flex items-center justify-between'>
-        <h3 className='text-gray-700'>
-          {auction.make} {auction.model}
-        </h3>
-        <p className='text-sm font-semibold'>{auction.year}</p>
-      </div>
-    </a>
-  );
+    return (
+        <Link href={`/auctions/details/${auction.id}`} className='group'>
+            <div className='w-full bg-gray-200 aspect-w-16 aspect-h-10 rounded-lg overflow-hidden'>
+                <div>
+                    <CarImage imageUrl={auction.imageUrl} />
+                    <div className='absolute bottom-2 left-2'>
+                        <CountdownTimer auctionEnd={auction.auctionEnd} />
+                    </div>
+                </div>
+            </div>
+            <div className='flex justify-between items-center mt-4'>
+                <h3 className='text-gray-700'>{auction.make} {auction.model}</h3>
+                <p className='font-semibold text-sm'>{auction.year}</p>
+            </div>
+
+        </Link>
+    )
 }
